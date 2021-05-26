59c066d4dff82dfadd94df66e80afafde07831db
Iman <seaweed.iman@gmail.com>
2021-05-26 16:12:57 +0800

    Updated the Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -12,13 +11,40 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div class="container-center-horizontal">
+      <div class="homepage screen">
+        <img
+          class="gamer"
+          src="https://anima-uploads.s3.amazonaws.com/projects/60adfd5bf3283a74a338dba5/releases/60ae0079c8124c55f90dc022/img/gamer@1x.png"
+        />
+        <div class="flex-col">
+          <h1 class="text-1">
+            MATCH UP
+            <br />
+            <br />
+            WIN MORE
+            <br />
+            <br />
+            HAVE FUN
+          </h1>
+          <div class="group-1">
+            <div class="overlap-group">
+              <div class="login">LOGIN</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// return (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>{!data ? "Loading..." : data}</p>
+//     </header>
+//   </div>
+// );
